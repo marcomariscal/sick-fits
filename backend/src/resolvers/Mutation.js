ff87ce5d202d6e9bfab4ba75bd8c5465608b9774
marco mariscal <marco.a.mariscal@gmail.com>
2019-11-26 07:22:46 -0500

    implement delete item functionality

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -28,6 +28,16 @@ const Mutations = {
 			},
 			info
 		);
+	},
+
+	async deleteItem(parent, args, ctx, info) {
+		const where = { id: args.id };
+		const item = await ctx.db.query.item({ where }, `{id title}`);
+		// find the item
+		// check if they own the item/have permissions
+		//TODO
+		// delete item
+		return ctx.db.mutation.deleteItem({ where }, info);
 	}
 };
 
