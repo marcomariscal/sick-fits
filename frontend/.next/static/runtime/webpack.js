7ddfa23d27ae7acc00805903b08e5613dbe07cfe
marco mariscal <marco.a.mariscal@gmail.com>
2019-12-28 09:28:39 -0500

    updating cart with cartItem attributes

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "7bd643ccad849574f470";
+/******/ 	var hotCurrentHash = "907f531cbcba7afa99e3";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- frontend/components/CreateItem.js
+++ frontend/components/CreateItem.js
@@ -28,11 +28,11 @@ const CREATE_ITEM_MUTATION = gql`
 
 class CreateItem extends Component {
 	state = {
-		title: "nice title",
-		description: "nice description",
-		image: "nice.jpg",
-		largeImage: "nicer.jpq",
-		price: 100
+		title: "",
+		description: "",
+		image: "",
+		largeImage: "",
+		price: 0
 	};
 
 	handleChange = e => {
@@ -54,7 +54,6 @@ class CreateItem extends Component {
 		);
 
 		const file = await res.json();
-		console.log(file);
 		this.setState({
 			image: file.secure_url,
 			largeImage: file.eager[0].secure_url
@@ -72,7 +71,6 @@ class CreateItem extends Component {
 							// call the mutation
 							const res = await createItem();
 							// change them to the single item page
-							console.log(res);
 							Router.push({
 								pathname: "/item",
 								query: { id: res.data.createItem.id }
